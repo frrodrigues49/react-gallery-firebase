da5b79685db53d70e477f90a3d82ab992e854570
Fagner <frodriguesbetim@gmail.com>
2021-10-10 18:26:16 -0300

    initial commit

---
+++ src/components/PhotoItem/index.tsx
@@ -0,0 +1,19 @@
+import * as C from './styles';
+
+type Props = {
+    url: string
+    name: string
+    onDelete: (name: string) => void;
+}
+
+const PhotoItem = ({ url, name, onDelete  }: Props) => {
+  return (
+      <C.Container>
+          <img src={url} alt={name} />
+          {name}
+          <button onClick={()=>onDelete(name)}>Excluir</button>
+      </C.Container>
+  )
+}
+
+export default PhotoItem;
